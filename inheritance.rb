7877d011a144b6f757a2f36c8c6c82543e8ed1e1
shivammahajan3 <shimahajan@bestpeers.in>
2024-12-09 10:53:32 +0530

    adding multilevel_hierarchical_inheritance

--- inheritance.rb
+++ inheritance.rb
@@ -21,5 +21,58 @@ class Dog < Animal
 end
 
 dog = Dog.new("Cheems")
-dog.eat
-dog.speak
+# dog.eat
+# dog.speak
+
+# Multilevel Inheritance 
+
+class Father
+    def name
+        puts "My Son Name is Salmon"
+    end
+end
+
+class Mother < Father
+    def surname
+        puts "Suraname is Bhoi"
+    end
+end
+
+class Child < Mother
+
+    def my_name
+        puts "My name is Salmon Bhoi"
+    end
+end
+
+c1 = Child.new
+# c1.name
+# c1.surname
+# c1.my_name
+
+
+# Hierarchical Inheritance 
+
+class Grandfather
+    def my_family
+        puts "My Family"
+    end
+end
+
+class Fatherr < Grandfather
+    def me
+        puts "It's Me"
+    end
+end
+
+class Son < Grandfather
+    def myself
+        puts "Only Me"
+    end
+end
+
+son = Son.new
+son.my_family
+
+father = Fatherr.new
+father.my_family

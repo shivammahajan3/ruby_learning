951ebc5d175a45b0a4804468ca403fe2d88dc62b
shivammahajan3 <shimahajan@bestpeers.in>
2024-12-24 11:41:21 +0530

    sum of the number in file number.txt

--- sum_of_number_in_file.rb
+++ sum_of_number_in_file.rb
@@ -1,3 +1,21 @@
 # Write a Ruby program that reads a file named "number.txt" containig a list of numbers,
 # one per line and calculates the sum of all the numbers
 
+numbers_file = File.new("number.txt", 'w+')
+
+loop do
+  puts "Enter a number or press 'Y' to stop:"
+  input = gets.chomp
+  break if input.downcase == 'y'
+
+  numbers_file.puts(input)
+end
+
+numbers_file.close 
+
+sum = 0
+File.foreach("number.txt") do |line|
+  sum += line.to_i
+end
+
+puts "The sum of all numbers is #{sum}"

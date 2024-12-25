79d9402ac814b62cb788f735a354bd2762840e43
shivammahajan3 <shimahajan@bestpeers.in>
2024-12-25 11:47:20 +0530

    count word frequency and value of the given category

--- count_word_frequency.rb
+++ count_word_frequency.rb
@@ -1,6 +1,23 @@
-def word_frequency(arr_string)
-    word_freq = arr_string.join(" ").downcase.split.tally
+class WordFrequency
+    attr_accessor :arr_string
+
+    def initialize(arr_string)
+        @arr_string = arr_string
+    end
+
+    def all_word_frequency
+        arr_string.join(' ').downcase.split.tally
+    end
+
+    def word_frequency(key)
+        all_word_frequency[key.downcase] || 0  # returns 0 if word is not found
+    end
 end
 
-sentences = ["Hello world", "hello Ruby", "Ruby is awesome", "World is big"]
-puts word_frequency(sentences)
+words_str = ["Hello world", "hello Ruby", "Ruby is awesome", "World is big"]
+sentence_arr = WordFrequency.new(words_str)
+
+puts sentence_arr.all_word_frequency   # => {"hello"=>2, "world"=>2, "ruby"=>2, "is"=>2, "awesome"=>1, "big"=>1}
+puts sentence_arr.word_frequency("hello")  # => 2
+puts sentence_arr.word_frequency("ruby")   # => 2
+puts sentence_arr.word_frequency("python") # => 0

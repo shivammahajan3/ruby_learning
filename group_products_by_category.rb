74f1532f1bc11294597ffd5f4535eaf00f6b4352
shivammahajan3 <shimahajan@bestpeers.in>
2024-12-26 17:45:06 +0530

    optimize the group product by category problem and use the module mixins for reusable code

--- group_products_by_category.rb
+++ group_products_by_category.rb
@@ -1,12 +1,12 @@
 require_relative 'product_grouping' 
 
 class ProductOrganizer
+
   include ProductGrouping
-  attr_accessor :result, :products
+  attr_accessor :result
 
   def initialize(products)
-    @products = products
-    @result = group_products_by_category_result
+    @result = group_products_by_category_result(products)
   end
 
 end
@@ -18,11 +18,11 @@ products = [
   { name: 'Jeans', price: 40, category: 'Clothing' },
   { name: 'Fridge', price: 500, category: 'Appliances' },
   { name: 'Microwave', price: 150, category: 'Appliances' }
-]
+] * 100000
 
 organizer = ProductOrganizer.new(products)
 p organizer.group_products_by_category
-p organizer.find_product_by_key('Electronics')
-p organizer.find_product_by_key('Clothing')
-p organizer.find_product_by_key('Electronicsrgjrfj')
+p organizer.find_product_by_category('Electronics')
+p organizer.find_product_by_category('Clothing')
+p organizer.find_product_by_category('Electronicsrgjrfj')
 
